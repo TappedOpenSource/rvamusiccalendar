6f810eef7adb8dd2db7bffc10fba1fd2c472eb61
Johannes Naylor <jonaylor89@gmail.com>
2023-11-29 22:30:28 -0500

    easter egg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 
 export default function Home() {
+  console.log('lick my balls bitch boi ;D');
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1
