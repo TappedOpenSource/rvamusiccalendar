0493de945fc96e7cbbc7d837fee39787e7d3e29b
Johannes Naylor <jonaylor89@gmail.com>
2023-11-29 23:28:14 -0500

    event stuff

--- src/app/ig/route.tsx
+++ src/app/ig/route.tsx
@@ -1,6 +1,24 @@
 import { ImageResponse } from "next/og";
 
+type Event = {
+    id: number,
+    created_at: string,
+    event_name: string,
+    description: string,
+    event_datetime: string,
+    door_time: string,
+    price: number,
+    url: string,
+    image_url: string,
+    source_id: string,
+    googleCalendarEventId: string,
+    venue: number;
+    stage: string,
+};
+
 export async function POST(req: Request) {
+    const { events } = await req.json() as { events: Event[] };
+    console.log({ events });
     return new ImageResponse(
         (
             <div
@@ -16,7 +34,16 @@ export async function POST(req: Request) {
 
                 }}
             >
-                <p>hello world</p>
+                <h1>RVA Music Calendar</h1>
+                {events.map((event, index) => (
+                    <div key={index}>
+                        <h2>{event.event_name}</h2>
+                        <h3>{event.stage}</h3>
+                        <h4>{event.event_datetime}</h4>
+                        <h5>{event.description}</h5>
+                        <h6>{event.price}</h6>
+                    </div>
+                ))}
             </div>
         ),
         {
