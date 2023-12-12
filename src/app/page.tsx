33204994da669bb17866f9a2561a08c09e8f89be
Johannes Naylor <jonaylor89@gmail.com>
2023-12-11 20:09:14 -0500

    update website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
 
+import Image from "next/image"
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1
-        className="text-4xl font-bold text-center"
-      >hey zephyr</h1>
-    </main>
+    <div className=" bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <main className="flex h-[80vh] flex-col items-center justify-center p-24">
+        <h1
+          className="text-4xl md:text-6xl font-extrabold text-center"
+        >RVA Music Calendar</h1>
+        <div className="h-4" />
+        <h3
+          className="text-2xl text-center"
+        >the source of truth for music events going on in Richmond, Virginia</h3>
+        <div className="h-4" />
+        <Link 
+          href="https://bit.ly/rvamusiccalendar"
+          className="bg-white font-bold text-black p-6 rounded-xl"
+        >add to your gcal!</Link>
+      </main>
+      <div className="flex justify-center items-center">
+        <Image
+          src="/images/calendar_sc.png"
+          alt="calendar screenshot"
+          width={1080}
+          height={1080}
+          className="rounded-xl hidden md:block"
+        />
+        <Image
+          src="/images/calendar_sc_small.png"
+          alt="calendar screenshot"
+          width={256}
+          height={1080}
+          className="rounded-xl md:hidden"
+        />
+      </div>
+      <div className="h-24" />
+    </div>
   )
 }
